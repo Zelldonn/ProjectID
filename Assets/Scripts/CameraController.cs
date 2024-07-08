9b933f6327b8c40e672944c5f4abc87237887513
Pierre <zeldonlast@gmail.com>
2024-07-08 19:39:32 +0200

    Update to Spherical coordinates

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,7 +8,7 @@ public class CameraController : MonoBehaviour
 {
     private GameObject _drone;
     public float CameraDistance = 3f;
-    public float _angleRad = 0f, _angleRadY = 0f;
+    public float _Theta = 0f, _Phi = 0f;
     void Start()
     {
         _drone = GameObject.FindGameObjectWithTag("Drone");
@@ -20,18 +20,13 @@ public class CameraController : MonoBehaviour
     {
         transform.LookAt(_drone.transform, Vector3.up);
 
-        _angleRad += Input.GetAxis("Mouse X") * Time.deltaTime * 5f;
-        _angleRadY += Input.GetAxis("Mouse Y") * Time.deltaTime * 5f;
+        _Phi += Input.GetAxis("Mouse X") * Time.deltaTime * 5f;
+        _Theta += Input.GetAxis("Mouse Y") * Time.deltaTime * 5f;
 
+        float x = CameraDistance * Mathf.Sin(_Theta) * Mathf.Cos(_Phi);
+        float y = CameraDistance * Mathf.Sin(_Theta) * Mathf.Sin(_Phi);
+        float z = CameraDistance * Mathf.Cos(_Theta)
 
-        float x_new = _drone.transform.localPosition.x + CameraDistance * Mathf.Cos(_angleRad);
-        float z_new = _drone.transform.localPosition.z + CameraDistance * Mathf.Sin(_angleRad);
-        float y_new = _drone.transform.localPosition.y + CameraDistance * Mathf.Cos(_angleRadY);
-
-        transform.position = new Vector3(x_new, y_new, z_new);
-        //transform.RotateAround(_drone.transform.position, Vector3.up, 20 * Time.deltaTime);
-
-
-
+        transform.position = new Vector3(x, z, y) + _drone.transform.position;
     }
 }
