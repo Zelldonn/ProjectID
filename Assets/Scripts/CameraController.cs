7d2ab9bcec6adfb40e4bad2942eae5e5ae8d054c
Zelldon <stringaripierre@hotmail.fr>
2024-07-10 13:48:39 +0200

    Improve camerra controller by adding lerp to look at function and by adding a max theta angle

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -11,7 +11,6 @@ public class CameraController : MonoBehaviour
     private GameObject _drone;
     private float CameraDistance = 3f, TargetCameraDistance = 3f;
     private float _Theta = 0.9834613f, _Phi = 4.5f;
-    
    
     void Start()
     {
@@ -34,20 +33,33 @@ public class CameraController : MonoBehaviour
         {
             _Phi += Input.GetAxis("Mouse X") * Time.deltaTime * 5f;
             _Theta -= Input.GetAxis("Mouse Y") * Time.deltaTime * 5f;
+
+            const float epsylon = 0.5f;
+            if(_Theta > Mathf.PI - epsylon) _Theta = Mathf.PI - epsylon;
+            if (_Theta < 0f + epsylon) _Theta = 0f + epsylon;
+
+            //if (_Phi > Mathf.PI * 2) _Phi = Mathf.PI * 2;
+            //if (_Phi < 0f) _Phi = 0f;
+
         }
     }
 
 
     private void UpdateCamera()
     {
-        transform.LookAt(_drone.transform, Vector3.up);
-
+        
         float x = CameraDistance * Mathf.Sin(_Theta) * Mathf.Cos(_Phi);
         float y = CameraDistance * Mathf.Sin(_Theta) * Mathf.Sin(_Phi);
         float z = CameraDistance * Mathf.Cos(_Theta);
 
         Vector3 targetPosition = new Vector3(x, z, y) + _drone.transform.position;
         transform.position = Vector3.Lerp(transform.position, targetPosition, .6f);
+
+        Vector3 targetCameraPosition = _drone.transform.position;
+        Vector3 newPos = Vector3.Lerp(_drone.transform.position, targetCameraPosition, .6f);
+
+        //transform.LookAt(_drone.transform, Vector3.up);
+        transform.LookAt(newPos, Vector3.up);
     }
     private void UpdateCameraDistance()
     {
