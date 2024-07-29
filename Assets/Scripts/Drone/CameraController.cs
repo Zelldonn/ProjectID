c5b45ad21b441bde28e71c7fe1c9fd1ce7293976
Zelldon <stringaripierre@hotmail.fr>
2024-07-29 19:47:52 +0200

    Fix camera stuttering (add FixedUpdate)

--- Assets/Scripts/Drone/CameraController.cs
+++ Assets/Scripts/Drone/CameraController.cs
@@ -27,7 +27,7 @@ public class CameraController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void LateUpdate()
+    void FixedUpdate()
     {
         UpdateCameraPosition();
         UpdateCameraDistance();
